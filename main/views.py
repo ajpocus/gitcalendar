a5a3dc0a32d398af9f917e09e259c769c84036c3
ajpocus <ajpocus@gmail.com>
2011-09-23 05:41:16 -0400

    Reworked caching to reflect view changes
    In rewriting the data views, I had removed caching of the entire commit_list.
    Now caching is back in place, for the repo list and each repo's commit list.
    To avoid problems with a repo named "repos", I used a key of "_repos" for the
    repo list cache. If this is a problem for you, please let me know.

--- main/views.py
+++ main/views.py
@@ -15,11 +15,18 @@ user = settings.USER
 urlbase = "https://api.github.com"
 
 def get_repos(request):
-    repourl = ''.join([urlbase, "/users/" + user + "/repos"])
-    repo_req = urllib2.Request(repourl)
-    opener = urllib2.build_opener()
-    repo_f = opener.open(repo_req)
-    return HttpResponse(repo_f.read(), mimetype="application/json")
+    # I use an underscore prefix in case someone has a repo named "repos"
+    repos = cache.get('_repos')
+
+    if not repos:
+	repourl = ''.join([urlbase, "/users/" + user + "/repos"])
+	repo_req = urllib2.Request(repourl)
+	opener = urllib2.build_opener()
+	repo_f = opener.open(repo_req)
+	repos = repo_f.read()
+	cache.set('_repos', repos, 30)
+
+    return HttpResponse(repos, mimetype="application/json")
 
 def get_commits(request):
     if 'repo' in request.GET:
@@ -27,25 +34,29 @@ def get_commits(request):
     else:
 	raise Http404
 
-    commiturl = ''.join([urlbase, '/repos/', user, '/', repo, "/commits"])
-    commit_req = urllib2.Request(commiturl)
-    opener = urllib2.build_opener()
-    commit_f = opener.open(commit_req)
-    commits = json.loads(commit_f.read())
+    commit_list = cache.get(repo)
 
-    commit_list = []
-    for commit in commits:
-	if commit['author']['login'] == "ajpocus":
-	    entry = {}
-	    timestring = commit['commit']['author']['date']
-	    entry['timestamp'] = time.mktime(
-		parse_date(timestring).timetuple())
-	    entry['repo'] = repo
-	    entry['message'] = commit['commit']['message'][:50]
-	    if len(entry['message']) == 50:
-		entry['message'] += '...'
+    if not commit_list:
+	commiturl = ''.join([urlbase, '/repos/', user, '/', repo, "/commits"])
+	commit_req = urllib2.Request(commiturl)
+	opener = urllib2.build_opener()
+	commit_f = opener.open(commit_req)
+	commits = json.loads(commit_f.read())
 
-	    commit_list.append(entry)
+	commit_list = []
+	for commit in commits:
+	    if commit['author']['login'] == "ajpocus":
+		entry = {}
+		timestring = commit['commit']['author']['date']
+		entry['timestamp'] = time.mktime(
+		    parse_date(timestring).timetuple())
+		entry['repo'] = repo
+		entry['message'] = commit['commit']['message'][:50]
+		if len(entry['message']) == 50:
+		    entry['message'] += '...'
+
+		commit_list.append(entry)
+	cache.set(repo, commit_list, 30)
 
     return HttpResponse(json.dumps(commit_list), mimetype="application/json")
 
